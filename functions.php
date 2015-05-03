e986f7b61d129f95ff5fb648f6c6f7418b1bdd69
artpi <artpi@post.pl>
2015-05-03 20:47:10 +0200

    Admin footer

--- functions.php
+++ functions.php
@@ -3,7 +3,12 @@ class WPhelpers {
     protected static $instance = null;
     private $welcomePanelContent;
     private $loginLogo='';
+    private $adminFooter='';
 
+    /**
+     * Singleton pattern. 
+     * @return self
+     */
     public static function getInstance() {
         if (!isset(self::$instance)) {
             self::$instance = new self();
@@ -11,6 +16,12 @@ class WPhelpers {
         return self::$instance;
     }
 
+    /**
+     * Remove unnecessary dashboard widgets in Admin panel
+     */
+    public function removeDashboardWidgets() {
+        add_action('wp_dashboard_setup', array($this, '_remove_dashboard_widgets'));
+    }
     function _remove_dashboard_widgets() {
         global $wp_meta_boxes;
         // Removing some dashboard mess
@@ -20,21 +31,28 @@ class WPhelpers {
     } 
 
 
-    function _welcome_panel() {
-        echo '<div class="welcome-panel-content">'.$this->welcomePanelContent.'</div>';
-    }
-
-
+    /**
+     * Add welcome panel to admin panel - used mostly for documentation.
+     * @param  [HTML] $content
+     */
     function welcomePanel($content) {
         $this->welcomePanelContent = $content; 
         remove_action('welcome_panel','wp_welcome_panel');
         add_action('welcome_panel', array($this, '_welcome_panel'));
     }
-
-    function removeDashboardWidgets() {
-        add_action('wp_dashboard_setup', array($this, '_remove_dashboard_widgets'));
+    function _welcome_panel() {
+        echo '<div class="welcome-panel-content">'.$this->welcomePanelContent.'</div>';
     }
 
+
+    /**
+     * Change logo in login site
+     * @param  [url] $logo
+     */
+    function changeLoginLogo($logo) {
+        $this->loginLogo = $logo;
+        add_action( 'login_enqueue_scripts', array($this, '_changeLoginLogo'));
+    }
     function _changeLoginLogo() {
     ?>
         <style type="text/css">
@@ -48,11 +66,19 @@ class WPhelpers {
     <?php 
     }
 
-    function changeLoginLogo($logo) {
-        $this->loginLogo = $logo;
-        add_action( 'login_enqueue_scripts', array($this, '_changeLoginLogo'));
+
+    /**
+     * Change footer in admin panel
+     * @param  [type] $footer [description]
+     */
+    function adminChangeFooter($footer) {
+        $this->adminFooter = $footer;
+        add_filter('admin_footer_text', array($this, '_adminChangeFooter'));
     }
+    function _adminChangeFooter () {
+        echo $this->adminFooter;
+    } 
 
-
+    
 
 }
